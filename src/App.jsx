fb55f5ebf7a2727422bed4618dc449f3068242e6
Demethrius Rodrigues <101345893+DemethriusSR@users.noreply.github.com>
2024-01-30 12:54:05 +0000

    Add 3 DIVs in Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
+import Button from './components/Button/Button'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+function Button (){
+    return(
+    <>
+        <div className= "button">
+            <button className="tag"> Button </button>
+        </div>      
+     </>
+    )
+}
+
+export default Button
